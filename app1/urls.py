53ddeabd7c22fcf43c6db4d6ad4ef517e7323fb9
khaledkamr <200013171@must.edu.eg>
2024-05-14 15:16:45 +0300

    Fixed login page path

--- app1/urls.py
+++ app1/urls.py
@@ -1,9 +1,10 @@
+from django.contrib import admin
 from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('login', views.login, name='login'),
+    path('signin', views.signin, name='signin'),
     path('movie', views.movie, name='movie'),
     path('movies', views.movies, name='movies'),
     path('register', views.register, name='register'),
